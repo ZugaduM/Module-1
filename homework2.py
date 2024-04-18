b1adce13b0170de7d8eda1de7abeba190a50a250
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-04-18 22:20:52 +0300

    Upload Homework 2

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = "Ilia"
+print(f'My name is: {name}')
+age = 35
+print(f'I`m {age} years old')
+new_age = age - 17
+print(f'I want i was {new_age} years old')
+is_student = True
+print(f'Is Student: {is_student}')
