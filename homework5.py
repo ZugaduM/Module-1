a4147461a497207b4c444b235778bb6f6b77aa9f
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-04-19 23:58:05 +0300

    Upload Homework 5

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'banana', 'tomato', 'potato', 'peach']
+print(f'List: {my_list}')
+print(f'First element: {my_list[0]}')
+print(f'Last element: {my_list[-1]}')
+print(f'Sublist: {my_list[3::5]}')
+my_list[2] = 'qiwi'
+print(f'Modified list: {my_list}\n')
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(f'Dictionary: {my_dict}')
+print(f'Translation: {my_dict['orange']}')
+my_dict.update({'kiwi': 'киви'})
+print(f'Modified dictionary: {my_dict}')
